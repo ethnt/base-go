02e500b4e4e296b9c599733f4a158677ce3a3238
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-09-03 15:15:07 -0400

    add mustache templates.

--- app.go
+++ app.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-    "github.com/hoisie/web"
+	"github.com/hoisie/web"
+	"github.com/hoisie/mustache"
 )
 
 func index(val string) string {
-	return "Hello world " + val
+	return mustache.RenderFileInLayout("views/index.mustache", "views/layouts/application.mustache", map[string]string{"c":"world"})
 }
 
 func main() {
-    web.Get("/(.*)", index)
-    web.Run("0.0.0.0:3000")
+	web.Get("/(.*)", index)
+	web.Run("0.0.0.0:3000")
 }
