970b9c5aab3ae05f51538fe55b4110c02dd96ea2
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-09-04 15:08:28 -0400

    organization.

--- app.go
+++ app.go
@@ -1,3 +1,19 @@
+/*
+	base-go
+		file: app.go
+*/
+
+/*
+	= Appendix
+		I. Imports
+		II. Helpers and Main
+		III. Controllers
+*/
+
+/*
+	I. Imports
+*/
+
 package main
 
 import (
@@ -5,11 +21,27 @@ import (
 	"github.com/hoisie/mustache"
 )
 
-func index(val string) string {
-	return mustache.RenderFileInLayout("views/index.mustache", "views/layouts/application.mustache", map[string]string{"c":"world"})
+
+/*
+	II. Helpers and Main
+*/
+
+func render(file string, params map[string]string) string {
+	return mustache.RenderFileInLayout(file, "views/layouts/application.mustache", params)
 }
 
 func main() {
 	web.Get("/(.*)", index)
 	web.Run("0.0.0.0:3000")
 }
+
+
+/*
+	III. Controllers
+*/
+
+func index(val string) string {
+	return render("views/index.mustache", map[string]string{"c":"world"})
+}
+
+
